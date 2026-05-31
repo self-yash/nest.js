b7c79ce207462d406980b80e2b057f762abe91ce
self-yash <yashmehta0005@gmail.com>
2026-05-31 18:29:28 +0530

    added swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
+  
   @Get()
+   @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of users returned successfully',
+  })
   getUsers() {
     return this.usersService.findAll();
   }
